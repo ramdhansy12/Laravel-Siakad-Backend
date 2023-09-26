a73fbd54560192b5696f58a93edce5acd6c3132a
RamdhanSyaifulloh <talladhan2772@gmail.com>
2023-09-26 11:51:41 +0700

    new page

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,26 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 
+
+Route::get('/login', function () {
+    return view('pages.auth.auth-login');
+})->name('login');
+
 Route::get('/', function () {
-    return view('pages.blank-page',['type_menu' => '']);
+    return view('pages.app.dashboard-siakad', ['type_menu' =>'']);
 });
+
+
+Route::get('/register', function () {
+    return view('pages.auth.auth-register');
+})->name('register');
+
+
+Route::get('/forgot', function () {
+    return view('pages.auth.auth-forgot-password');
+})->name('forgot');
+
+
+Route::get('/reset-password', function () {
+    return view('pages.auth.auth-reset-password');
+})->name('reset-password');
