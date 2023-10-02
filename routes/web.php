821ee78ccf8fd65cc19463c164c6919b6603479c
RamdhanSyaifulloh <talladhan2772@gmail.com>
2023-10-02 19:39:26 +0700

    CRUD User

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+
+
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,6 +26,8 @@ Route::middleware(['auth'])->group(function(){
     Route::get('home', function () {
         return view('pages.app.dashboard-siakad', ['type_menu' =>'']);
     })->name('home');
+    Route::resource('user', UserController::class);
+    
 });
 
 // Route::get('/register', function () {
